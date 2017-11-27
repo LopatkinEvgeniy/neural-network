2f3c5222c1bc41fd731bfdecd57b1a53e5ae24d9
elopatkin <javany25@mail.ru>
2017-11-27 07:47:08 +0300

    simplify iris data preparation

--- iris.py
+++ iris.py
@@ -2,47 +2,25 @@ import csv
 import random
 from lib.lib import Neuron, Network
 
-setosa = 'setosa'
-versicolor = 'versicolor'
-virginica = 'virginica'
-
-indexes = {
-    setosa: [1, 0, 0],
-    versicolor: [0, 1, 0],
-    virginica: [0, 0, 1]
+iris_codes = {
+    'setosa': [1, 0, 0],
+    'versicolor': [0, 1, 0],
+    'virginica': [0, 0, 1]
 }
 
-iris_dataset={}
-iris_dataset[setosa] = []
-iris_dataset[versicolor] = []
-iris_dataset[virginica] = []
+iris_dataset = []
 
 for i, row in enumerate(csv.reader(open('data/iris.csv', 'r'))):
     if i == 0:
         continue
-    data = list(map(lambda n: float(n), row[0:4]))
-    iris_dataset[row[4]].append(data)
+    iris_name = row[4]
+    iris_data = list(map(lambda n: float(n), row[0:4]))
+    iris_dataset.append([iris_data, iris_codes[iris_name]])
 
-random.shuffle(iris_dataset[setosa])
-random.shuffle(iris_dataset[versicolor])
-random.shuffle(iris_dataset[virginica])
+random.shuffle(iris_dataset)
 
-validation_data = []
-train_data = []
-
-for i in range(5):
-    validation_data.append([iris_dataset[setosa][i], indexes[setosa]])
-    validation_data.append([iris_dataset[versicolor][i], indexes[versicolor]])
-    validation_data.append([iris_dataset[virginica][i], indexes[virginica]])
-
-for iris_name in iris_dataset:
-    for i, data in enumerate(iris_dataset[iris_name]):
-        if i < 5:
-            continue
-        train_data.append([data, indexes[iris_name]])
-
-random.shuffle(validation_data)
-random.shuffle(train_data)
+validation_data = iris_dataset[0:15]
+train_data = iris_dataset[15:]
 
 network = Network([
     [Neuron(4), Neuron(4), Neuron(4), Neuron(4), Neuron(4), Neuron(4), Neuron(4), Neuron(4)],
